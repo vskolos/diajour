b8b07e2fd52bf204745e5b0949cfc798c446042b
Vsevolod Kolos <vskolos@icloud.com>
2023-03-14 10:21:35 +0300

    Add default datetime and glucose input focus

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,26 +1,32 @@
-import { type FormEvent, useState } from 'react'
+import { useState, type FormEvent } from 'react'
 
-import type { NextPage } from 'next'
+import { Header } from '@/partials'
+import { formatDate } from '@/utils'
 import { api } from '@/utils/api'
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { Header } from '@/partials'
 import { toast } from 'react-hot-toast'
-import { formatDate } from '@/utils'
 
 const EditEntry: NextPage = () => {
   const router = useRouter()
   const id = router.query.id as string
+  const isNew = id === 'new'
 
-  const [date, setDate] = useState('')
-  const [timePeriod, setTimePeriod] = useState<TimePeriod>('morning')
+  const now = new Date()
+  const currentHour = now.getHours()
+
+  const [date, setDate] = useState(formatDate(now))
+  const [timePeriod, setTimePeriod] = useState<TimePeriod>(
+    currentHour < 12 ? 'morning' : currentHour < 18 ? 'midday' : 'evening'
+  )
   const [glucoseAmount, setGlucoseAmount] = useState('')
   const [insulinDosage, setInsulinDosage] = useState('')
   const [weight, setWeight] = useState('')
 
   api.data.getById.useQuery(id, {
     onSuccess: (data) => {
-      if (!data) return id !== 'new' && void router.push('/new')
+      if (!data) return !isNew && void router.push('/new')
 
       setDate(formatDate(data.date))
       setTimePeriod(data.timePeriod as TimePeriod)
@@ -60,26 +66,26 @@ const EditEntry: NextPage = () => {
       weight: weight ? Number(weight) : null,
     }
 
-    id === 'new' ? addDataEntry(input) : editDataEntry(input)
+    isNew ? addDataEntry(input) : editDataEntry(input)
   }
 
   return (
     <>
       <Head>
-        <title>{id === 'new' ? 'Добавить' : 'Редактировать'} замер</title>
+        <title>{isNew ? 'Добавить' : 'Редактировать'} замер</title>
         <meta name="description" content="Редактирование замера глюкозы" />
       </Head>
       <Header />
       <main className="grid gap-2">
         <div className="grid overflow-hidden rounded-xl">
           <h1 className="bg-zinc-700 px-6 py-4 text-xl font-semibold text-zinc-100">
-            {id === 'new' ? 'Добавить' : 'Редактировать'} замер
+            {isNew ? 'Добавить' : 'Редактировать'} замер
           </h1>
           <form className="grid bg-zinc-600" onSubmit={handleEditDataEntry}>
             <label className="grid grid-cols-4 items-center gap-8 px-6 py-3.5 even:bg-zinc-700">
               <span className="text-lg font-medium text-zinc-100">Дата</span>
               <input
-                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
+                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-left text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
                 type="date"
                 placeholder="ДД.ММ.ГГГГ"
                 value={date}
@@ -89,7 +95,7 @@ const EditEntry: NextPage = () => {
             <label className="grid grid-cols-4 items-center gap-8 px-6 py-3.5 even:bg-zinc-700">
               <span className="text-lg font-medium text-zinc-100">Период</span>
               <select
-                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
+                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-left text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
                 value={timePeriod}
                 onChange={(e) => setTimePeriod(e.target.value as TimePeriod)}
               >
@@ -101,7 +107,7 @@ const EditEntry: NextPage = () => {
             <label className="grid grid-cols-4 items-center gap-8 px-6 py-3.5 even:bg-zinc-700">
               <span className="text-lg font-medium text-zinc-100">Глюкоза</span>
               <input
-                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
+                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-left text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
                 type="text"
                 inputMode="decimal"
                 placeholder="16.5"
@@ -113,12 +119,13 @@ const EditEntry: NextPage = () => {
                       .replaceAll(/[^0-9\.]/g, '')
                   )
                 }
+                ref={(input) => input && input.focus()}
               />
             </label>
             <label className="grid grid-cols-4 items-center gap-8 px-6 py-3.5 even:bg-zinc-700">
               <span className="text-lg font-medium text-zinc-100">Инсулин</span>
               <input
-                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
+                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-left text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
                 type="text"
                 inputMode="decimal"
                 placeholder="2.5"
@@ -135,7 +142,7 @@ const EditEntry: NextPage = () => {
             <label className="grid grid-cols-4 items-center gap-8 px-6 py-3.5 even:bg-zinc-700">
               <span className="text-lg font-medium text-zinc-100">Вес</span>
               <input
-                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
+                className="col-span-3 border border-transparent border-b-zinc-500 bg-transparent text-left text-lg text-zinc-200 placeholder:text-zinc-500 focus-visible:border-b-zinc-300 focus-visible:outline-none"
                 type="text"
                 inputMode="decimal"
                 placeholder="3.5"
@@ -158,7 +165,7 @@ const EditEntry: NextPage = () => {
             </button>
           </form>
         </div>
-        {id !== 'new' && (
+        {!isNew && (
           <button
             className="justify-self-center border-b border-red-600 text-red-600 hover:border-red-500 hover:text-red-500 focus-visible:border-red-500 focus-visible:text-red-500 active:border-red-600 active:text-red-600"
             onClick={() => removeDataEntry(id)}
