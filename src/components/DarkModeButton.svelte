2559c5ed53739d082901d788e7096bc7c55713d1
Vsevolod Kolos <vskolos@icloud.com>
2023-08-17 23:45:29 +0300

    add button type to prevent forms from submitting

--- src/components/DarkModeButton.svelte
+++ src/components/DarkModeButton.svelte
@@ -14,6 +14,7 @@
 </script>
 
 <button
+  type="button"
   class="p-1.5 border-[1.5px] border-neutral-300 rounded-lg hover:bg-neutral-200 focus-visible:bg-neutral-200 active:bg-neutral-300 dark:border-neutral-600 dark:hover:bg-neutral-700 dark:focus-visible:bg-neutral-700 dark:active:bg-neutral-600 transition-colors"
   aria-label="Темная тема"
   on:click={toggleDarkMode}
